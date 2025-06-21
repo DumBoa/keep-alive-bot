9e1eba21a1099bc3bda21649be4d8b661dc84e80
Dao Huy Hoang <150949641+DumBoa@users.noreply.github.com>
2025-06-21 08:56:18 +0700

    index.js

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const puppeteer = require("puppeteer");
   await page.goto("https://80-firebase-app-1750422867613.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev/vnc.html?autoconnect=true&resize=remote");
   console.log("Đã vào VNC. Đang giữ session...");
 
-  await page.waitForTimeout(10 * 60 * 1000); // 10 phút
+  await page.waitForTimeout(10 * 10 * 1000); //1p
   await browser.close();
 })();
